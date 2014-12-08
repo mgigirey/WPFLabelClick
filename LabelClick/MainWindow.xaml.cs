2e4f4ffab31fadd43935ff0fb81fa392d204daf1
Marcelo Gigirey <mgigirey@gmail.com>
2014-12-08 11:30:22 +0200

    .gitignore added

---
+++ LabelClick/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace LabelClick
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //DataContext = new MyViewModel();
+        }
+
+        private void lblNext_TouchDown(object sender, TouchEventArgs e)
+        {
+            MessageBox.Show("Clicked from Code Behind");
+        }
+    }
+}
---
+++ LabelClick/MyViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace LabelClick
+{
+    public class MyViewModel : ObservableObject
+    {
+        private ICommand labelClicked { get; set; }
+
+        public ICommand LabelClicked
+        {
+            get
+            {
+                if (labelClicked == null)
+                {
+                    labelClicked = new RelayCommand(
+                        param => TakeAction()
+                        );
+                }
+                return labelClicked;
+            }
+        }
+
+        private void TakeAction()
+        {
+            MessageBox.Show("Clicked from ViewModel");
+        }
+    }
+}
